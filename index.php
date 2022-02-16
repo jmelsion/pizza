2a844f2a413c96246a8b4c0dce20f1651e0c11f1
jmelsion <99830161+jmelsion@users.noreply.github.com>
2022-02-16 19:05:51 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+
+<?php include('templates/header.php'); ?>
+
+<?php include('templates/footer.php'); ?>
+
+
+</html>
---
+++ templates/footer.php
@@ -0,0 +1,14 @@
+
+
+<!-- No hace falta poner el link a materialize ni nada "declarado" en header.php
+puesto que realmente todo formará parte de una misma página -->
+
+
+
+
+
+
+<footer class = "section">
+    <div class="center grey-text">Copyright DWEC Pizza</div>
+</footer>
+</body>
